193c70111a572665a683e7d0ab732bd5b40a55cc
GehanLiyanage2002 <iit22002.std.uwu.ac.lk>
2025-03-23 01:31:49 +0530

    Navbar Design

--- WebMaster/src/assets/Components/Header/Header.jsx
+++ WebMaster/src/assets/Components/Header/Header.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import './Header.css'
+import logo from './react.svg'
+import MenuLink from '../MenuLink/MenuLink'
+
 
 function Header() {
   return (
     <div id="navcontent">
-        <h3>Header Content</h3>
+      <a href='https://reactjs.org' target='_blank' rel='noreferrer noopener'>
+        <img src={logo} alt='React Logo' /></a>
+        <div>
+          <MenuLink linkname="Home" url="#home"/>
+          <MenuLink linkname="About" url="#about"/>
+          <MenuLink linkname="Services" url="#services"/>
+          <MenuLink linkname="Contact" url="#contact"/>
+        </div>
     </div>
   )
 }
